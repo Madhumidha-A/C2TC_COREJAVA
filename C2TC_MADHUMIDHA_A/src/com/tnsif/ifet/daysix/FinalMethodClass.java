f4ffea9fd9060ebabdd01da789c1d9b82e72bc41
madhu <madhu@Madhumidha>
2024-11-06 11:51:06 +0530

    update

---
+++ C2TC_MADHUMIDHA_A/src/com/tnsif/ifet/daysix/FinalMethodClass.java
@@ -0,0 +1,22 @@
+package com.tnsif.ifet.daysix;
+
+public class FinalMethodClass {
+	FinalMethodClass() {
+		System.out.println("This is a default constructor");
+	}
+
+	final int a = 50;
+
+	// Final method
+	final void show() {
+		System.out.println("Value of a: " + a);
+	}
+}
+public class FinalMethodClassTest extends FinalMethodClass  {
+	  // Compile time error because we cannot override the final method 
+		/*
+		 * @Override void show() {
+		 * 
+		 * System.out.println("This is the final method of FinalMethodEx class"); }
+		 */
+	   
