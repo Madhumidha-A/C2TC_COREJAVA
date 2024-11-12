f59560fdd5faa91b4ee41ad099e1ace49c0dddc5
madhu <madhu@Madhumidha>
2024-11-12 19:22:16 +0530

    update

---
+++ C2TC_MADHUMIDHA_A/src/com/tnsif/ifet/daytwelve/InvalidMarksException.java
@@ -0,0 +1,17 @@
+package com.tnsif.ifet.daytwelve;
+
+public class InvalidMarksException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public InvalidMarksException() {
+		super();
+	}
+
+	public InvalidMarksException(String message) {
+		super(message);
+	}
+
+}
